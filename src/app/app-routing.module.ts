3685773808313b546196caf88d95adecd36d4efa
inigofede <fede.reveck@gmail.com>
2023-05-15 01:50:36 -0300

    Finalizacion FrontEnd
    Front Funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
 
 
 const routes: Routes = [
@@ -11,8 +13,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'nuevaexp', component: NewExperienciaComponent},
   { path: 'editexp/:id', component: EditExperienciaComponent},
- // { path: 'nuevaedu', component: NewEducacionComponent},
- // { path: 'editedu/:id', component: EditEducacionComponent},
+  { path: 'nuevaedu', component: NewEducacionComponent},
+  { path: 'editedu/:id', component: EditEducacionComponent},
 ];
 
 @NgModule({
